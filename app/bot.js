b1c5bc229d16447fba146c2b78de327b41840c05
Masih IT <smasihsajadi@gmail.com>
2019-12-11 23:23:34 +0330

    commit

--- app/bot.js
+++ app/bot.js
@@ -1,8 +1,4 @@
-// const config = require('./config');
-const config = {
-  port: 5656,
-  apiToken: '811208007:AAEHhcp2-ROMhHtRcTnc4q29sgJeuFFJHn0'
-};
+const config = require('./config');
 
 const telegraf = require('telegraf');
 const bot = new telegraf(config.apiToken);
