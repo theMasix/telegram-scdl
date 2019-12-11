423e818a4c0f5b47bd05d8c6cd4b7226bd2ca51c
Masih IT <smasihsajadi@gmail.com>
2019-12-11 22:44:40 +0330

    a nwe commit

--- public/bot.js
+++ public/bot.js
@@ -1,9 +1,10 @@
-const config = require('./config');
-const telegraf = require('telegraf');
+// const config = require('./config');
+// const telegraf = require('telegraf');
 
-const bot = new telegraf(config.apiToken);
+// const bot = new telegraf(config.apiToken);
 
-bot.start(ctx => {
-  ctx.reply('Welcome');
-});
-bot.launch();
+// bot.start(ctx => {
+//   ctx.reply('Welcome');
+// });
+// bot.launch();
+console.log('Hi Heroku!');
