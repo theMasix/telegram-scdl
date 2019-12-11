f851aa9848d132b41181a2a1e57a047fcc8a7c54
Masih IT <smasihsajadi@gmail.com>
2019-12-11 23:56:06 +0330

    add express

--- app/bot.js
+++ app/bot.js
@@ -1,11 +1,22 @@
 const config = require('./config');
 
-const telegraf = require('telegraf');
-const bot = new telegraf(config.apiToken);
+const Telegraf = require('telegraf');
+const express = require('express');
 
+const bot = new Telegraf(config.apiToken);
+// Set the bot response
 bot.start(ctx => {
-  ctx.reply('Welcome');
+  ctx.reply('Hi');
 });
-bot.launch();
 
-console.log('Bot is working!');
+// Set telegram webhook
+// npm install -g localtunnel && lt --port 3000
+bot.telegram.setWebhook('https://scloud-dl-bot.herokuapp.com/app');
+
+const app = express();
+app.get('/', (req, res) => res.send('Hello World!'));
+// Set the bot API endpoint
+app.use(bot.webhookCallback('/app'));
+app.listen(config.port, () => {
+  console.log(`Example app listening on port ${config.port}!`);
+});
