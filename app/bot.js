6b34595191b9ce10d6537527bf8f6fc1591bce64
Masih IT <smasihsajadi@gmail.com>
2019-12-11 22:53:55 +0330

    hey there!

--- app/bot.js
+++ app/bot.js
@@ -1,6 +1,10 @@
-const config = require('./config');
-const telegraf = require('telegraf');
+// const config = require('./config');
+const config = {
+  port: 5656,
+  apiToken: '811208007:AAEHhcp2-ROMhHtRcTnc4q29sgJeuFFJHn0'
+};
 
+const telegraf = require('telegraf');
 const bot = new telegraf(config.apiToken);
 
 bot.start(ctx => {
