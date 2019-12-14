3d81127038a9f1dd27521463e12e43894acbfef6
Masih IT <smasihsajadi@gmail.com>
2019-12-14 10:23:30 +0330

    another commit

--- AAEHhcp2-ROMhHtRcTnc4q29sgJeuFFJHn0/scdl.js
+++ AAEHhcp2-ROMhHtRcTnc4q29sgJeuFFJHn0/scdl.js
@@ -5,25 +5,30 @@ let spawn = require('child_process').spawn;
 let scdl = {};
 scdl.getTrackInfo = url => {
   return new Promise((resolve, reject) => {
-    let options = ['-o', '%(uploader)s - %(title)s.%(ext)s', '-j', url];
-    let process = spawn('youtube-dl', options);
+    try {
+      let options = ['-o', '%(uploader)s - %(title)s.%(ext)s', '-j', url];
+      let process = spawn('youtube-dl', options);
 
-    let buffer = '';
-    process.stdout.on('data', data => {
-      buffer += decoder.write(data);
-    });
-    process.stdout.on('end', () => {
-      buffer += decoder.end();
-      let parse = JSON.parse(buffer);
-      let trackName = parse.uploader + ' - ' + parse.fulltitle;
+      let buffer = '';
+      process.stdout.on('data', data => {
+        buffer += decoder.write(data);
+      });
+      process.stdout.on('end', () => {
+        buffer += decoder.end();
+        let parse = JSON.parse(buffer);
+        let trackName = parse.uploader + ' - ' + parse.fulltitle;
 
-      let result = {
-        url: parse.url,
-        trackName: trackName,
-        thumbnail: parse.thumbnail
-      };
-      return resolve(result);
-    });
+        let result = {
+          url: parse.url,
+          trackName: trackName,
+          thumbnail: parse.thumbnail
+        };
+        return resolve(result);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
     process.on('error', err => {
       console.log(err);
     });
